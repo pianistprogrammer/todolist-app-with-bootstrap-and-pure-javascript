9fd8f5a897fa5c61081ee884f3882b50380037f1
Abimbola Jeremiah <jeremiah.abimbola@silveredgeprojects.com>
2017-09-04 11:48:33 +0100

    commit

--- js/todo.js
+++ js/todo.js
@@ -1,32 +1,8 @@
 
 window.onload = function(){
-  var list = document.getElementsByTagName("LI");
-for (var i = 0; i < list.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  list[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-for (var i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Use the enter button to alternatively add a new task
+  showList()
+ 
+ // Use the enter button to alternatively add a new task
 document.getElementById("taskInput").onkeydown = function(e) {
    if (e.keyCode == 13){
     newTask(); 
@@ -34,31 +10,66 @@ document.getElementById("taskInput").onkeydown = function(e) {
   };
 
 }
-
-
-// Create a new list item when clicking on the "Add" button
-function newTask() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("taskInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You haven't added anything");
-  } else {
-    document.getElementById("uList").appendChild(li);
-  }
-  document.getElementById("taskInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (var i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+ function get_todos() {
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str); 
     }
-  }
+    return todos;
 }
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos));
+ 
+    showList();
+    return false;
+}
+// Create a new list item when clicking on the "Add" button
+function showList(){
+ var todos = get_todos();
+    
+    var html = '<ul>';
+    for(var i=0; i<todos.length; i++) {
+        html += '<li class="list">' + todos[i] + '<span class="close" id="' + i  + '">x</span></li>';
+
+    };
+    html += '</ul>';
+    document.getElementById('uList').innerHTML = html;
+ 
+    var buttons = document.getElementsByClassName('close');
+    for (var i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+
+    };
+    var lists = document.querySelectorAll('.list');
+    for (var i=0; i < lists.length; i++) {
+        lists[i].addEventListener('click', function(ev) {
+          if (ev.target.tagName === 'LI') {
+            ev.target.classList.toggle('checked');
+          }
+        
+        },false);
+
+    };
+}
+function newTask() {
+    var inputValue = document.getElementById("taskInput").value;
+     if (inputValue === '') {
+        alert("You haven't added anything");
+      } else {
+         var todos = get_todos();
+        todos.push(inputValue);
+        localStorage.setItem('todo', JSON.stringify(todos));
+     
+        showList();
+        document.getElementById("taskInput").value = "";
+        return false;
+      }
+   
+}
+ 
+
+ 
